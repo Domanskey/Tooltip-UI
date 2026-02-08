9d5885600d7427c4b62414f317a3f603f87db0c8
domanskey <domadam04@gmail.com>
2026-02-08 15:59:24 +0100

    feat: add static styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import "./index.css";
 
 function App() {
   return (
-    <>
-      <h1 className="bg-amber-300">yolo</h1>
-    </>
+    <nav>
+      <ul className="flex flex-row justify-between gap-4 list-none">
+        <li>
+          <button className="cursor-pointer">Home</button>
+        </li>
+        <li className="before:content-['•'] before:pr-4 before:text-gray-400">
+          <button className="cursor-pointer">Projects</button>
+        </li>
+        <li className="before:content-['•'] before:pr-4 before:text-gray-400">
+          <button className="cursor-pointer">Blog</button>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
